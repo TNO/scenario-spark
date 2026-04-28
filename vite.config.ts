19b8b05629c5c806b61d8fed1b3e1bd8c831cd32
Erik Vullings <erik.vullings@tno.nl>
2026-04-28 18:17:01 +0200

    fix: path prefix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  base: '/scenario-spark/',
   publicDir: 'public',
 
   build: {
